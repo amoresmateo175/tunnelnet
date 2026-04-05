d9ac134fb8f7228125dcb26ad4771093840e8560
Mateo <amoresmateo175@gmail.com>
2026-04-05 11:03:04 -0400

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,19 @@
 import "./globals.css";
+import { ReactNode } from "react";
+import type { Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "TunnelNet | Managed Network Services",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
